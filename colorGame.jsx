5fe1249ec26ba5e4cf3e80c650425645e367a628
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-03-02 23:18:15 +0800

    Update colorGame.jsx

--- colorGame.jsx
+++ colorGame.jsx
@@ -10,66 +10,67 @@ const Square = ({ takeTurn, id, player }) => {
     // function that takes the player as a parameter and then uses it in the calculation to setColor
     // but does NOT update the player held in the state of the Board component
     const innerTakeTurn = (id) => {
-      return ((player + 1) % 2);
+        return ((player + 1) % 2);
     };
   
     return (
-      <button
-        id={id}
-        onClick={(e) => {
-          setColor(innerTakeTurn(id));
-          e.preventDefault();
-          e.target.style.background = palet[color];
-        }}
-      ></button>
+        <button
+            id={id}
+            onClick={(e) => {
+                //setColor(innerTakeTurn(id));
+                setColor(takeTurn(id));
+                e.preventDefault();
+                e.target.style.background = palet[color];
+            }}
+        ></button>
     );
-  };
+};
   
-  const Board = () => {
+const Board = () => {
     // 1st player is 1
     // State keeps track of next player
     const [player, setPlayer] = React.useState(1);
     console.log('Board re-rendering now.');
-  
+
     // check for winner (see superset.js)
     let status = `Player ${player}`;
     console.log(`Status Player ${status}`);
-  
+
     // Note that Child (Square Component) calls this function
     // However the function has access to the player held here
     const takeTurn = (id) => {
-      setPlayer((player + 1) % 2); // get next player
-      return player;
+        setPlayer((player + 1) % 2); // get next player
+        return player;
     };
     function renderSquare(i, color) {
-      // use properties to pass callback function takeTurn to Child
-      return <Square takeTurn={takeTurn} id={i} player={player}></Square>;
+        // use properties to pass callback function takeTurn to Child
+        return <Square takeTurn={takeTurn} id={i} player={player}></Square>;
     }
     return (
-      <div className="game-board">
-        <div className="grid-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
+        <div className="game-board">
+            <div className="grid-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div id="info">
+                <h1>{status}</h1>
+                <button onClick={() => takeTurn()}> Change Player</button>
+            </div>
         </div>
-        <div id="info">
-          <h1>{status}</h1>
-          <button onClick={() => takeTurn()}> Change Player</button>
-        </div>
-      </div>
     );
-  };
+};
   
-  const Game = () => {
+const Game = () => {
     return (
-      <div className="game">
-        <Board></Board>
-      </div>
+        <div className="game">
+            <Board></Board>
+        </div>
     );
-  };
+};
   
-  // ========================================
+// ========================================
   
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const element = <App/>
+const element = <Game/>
 root.render(element);
